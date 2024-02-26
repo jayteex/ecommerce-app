94b04c3094827cd27b977f552a711d1b5484ef4e
Julian T <julian.thiel@outlook.com>
2024-02-26 19:12:48 +0100

    Add Render backend deployment URL

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -4,4 +4,4 @@ const isLocalDevelopment = process.env.NODE_ENV === 'development';
 // Set the backend URL based on the environment
 export const HOST = isLocalDevelopment
   ? "http://localhost:3000" // Local development URL
-  : "https://your-backend-service.onrender.com"; // Render deployment URL
+  : "https://ecommerce-app-backend-kwni.onrender.com"; // Render deployment URL
