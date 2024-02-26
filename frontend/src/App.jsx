0316ac3d7cef607955cdc1f56495d311fab5354c
Julian T <julian.thiel@outlook.com>
2024-02-26 20:31:08 +0100

    Update pictures

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Listing } from './features/listing/Listing';
 import { Cart } from './features/cart/Cart';
 import Search from './features/search/Search';
 import "../index.css";
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, Navigate } from 'react-router-dom';
 import Root from './features/navigation/Root';
 import About from './features/navigation/About';
 import SignIn from './features/login/SignIn';
@@ -23,6 +23,7 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Root />}>
     <Route path="about" element={<About />} />
     <Route path="sign-in" element={<SignIn />} />
+    <Route path="/" element={<Navigate to="/home" replace />} />
     <Route path="home" element={<ListingWrapper />} />
   </Route>
 )
