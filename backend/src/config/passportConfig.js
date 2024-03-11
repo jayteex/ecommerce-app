e3f4dd3bdd3878bbf9592df23189ec5754962acb
Julian T <julian.thiel@outlook.com>
2024-03-11 23:50:21 +0100

    Minor changes

--- backend/src/config/passportConfig.js
+++ backend/src/config/passportConfig.js
@@ -2,6 +2,9 @@
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 const supabase = require('./supabase');
+const redis = require('redis');
+const redisClient = redis.createClient();
+require('dotenv').config();
 
 const initialize = (passport) => {
   passport.use(new LocalStrategy({ usernameField: 'email', passwordField: 'password' }, async (email, password, done) => {
@@ -32,38 +35,58 @@ const initialize = (passport) => {
     }
   }));
 
-// Serialize user object based on environment
-if (process.env.NODE_ENV === 'production') {
-  const { serializeUser, deserializeUser } = require('./redisUtils');
-  passport.serializeUser(serializeUser);
-  passport.deserializeUser(deserializeUser);
-} else {
-  // Use default serialization for local environment
-  passport.serializeUser((user, done) => {
-    console.log('Serialized User:', user);
-    done(null, user.customerid);
-  });
-
-  passport.deserializeUser(async (id, done) => {
-    try {
-      const { data: user, error } = await supabase
-        .from('customers')
-        .select('customerid, email, firstname, lastname, city, address')
-        .eq('customerid', id)
-        .single();
-
-      if (error) throw error;
-
-      if (!user) return done(null, false);
-
-      console.log('Deserialized User:', user);
-      done(null, user);
-    } catch (err) {
-      console.error(`Deserialization Error: ${err.message}`);
-      done(err);
+  // Serialize user object based on environment
+  if (process.env.NODE_ENV === 'production') {
+    // Serialize user object and store in Redis session
+    function serializeUserRedis(user, done) {
+      console.log('Serialized User:', user);
+      done(null, user.customerid);
     }
-  });
-}
+
+    passport.serializeUser(serializeUserRedis);
+
+    // Deserialize user object and retrieve from Redis session
+    function deserializeUserRedis(id, done) {
+      redisClient.get(id, (err, user) => {
+        if (err) {
+          console.error(`Deserialization Error: ${err.message}`);
+          return done(err);
+        }
+        if (!user) return done(null, false);
+        console.log('Deserialized User:', user);
+        done(null, user);
+      });
+    }
+
+    passport.deserializeUser(deserializeUserRedis);
+
+  } else {
+    // Use default serialization for local environment
+    passport.serializeUser((user, done) => {
+      console.log('Serialized User:', user);
+      done(null, user.customerid);
+    });
+
+    passport.deserializeUser(async (id, done) => {
+      try {
+        const { data: user, error } = await supabase
+          .from('customers')
+          .select('customerid, email, firstname, lastname, city, address')
+          .eq('customerid', id)
+          .single();
+
+        if (error) throw error;
+
+        if (!user) return done(null, false);
+
+        console.log('Deserialized User:', user);
+        done(null, user);
+      } catch (err) {
+        console.error(`Deserialization Error: ${err.message}`);
+        done(err);
+      }
+    });
+  }
 
 };
 
